eee291a3275f26f3079c6969557fefda2df876aa
Akem-Ben <akemini.ndaobong@gmail.com>
2023-10-03 00:57:57 +0100

    folders layout

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
+
+const app = express()
+dotenv.config()
+const Port = process.env.PORT
+
+
+
+
+app.listen(Port, ()=>{
+    console.log(`App is listening on Port ${Port}`)
+})
