335599ee7ae43637ddb888e62176169cc91e22bd
Akem-Ben <akemini.ndaobong@gmail.com>
2023-10-03 01:39:18 +0100

    database setup

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
+import {database} from './configuration/database'
 const app = express()
 dotenv.config()
 const Port = process.env.PORT
 
 
+database.sync({}).then( ()=>{
+    console.log(`Database connected`)
+}).catch((err)=>{
+    console.log(err)
+})
 
 
 app.listen(Port, ()=>{
